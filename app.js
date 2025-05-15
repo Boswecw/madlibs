9e173e5c2d5c55c89de55c281be8882e06cde039
Charles Boswell <charliewboswell@gmail.com>
2025-05-15 19:26:02 -0400

    seventh push

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ var word15 = document.getElementById('word15').value;
 var word16 = document.getElementById('word16').value;
 var word17 = document.getElementById('word17').value;
 var word18 = document.getElementById('word18').value;
+var word19 = document.getElementById('word19').value;
 
-var paragraph = `A vacation is when you take a trip to some ${word1} place with your ${word2} family. Usually you go to some place that is near a/an ${word3} or up on a/an ${word4}. A good vacation place is one where you can ride ${word1} or play ${word5} or go hunting for ${word6}. I like to spend my time ${word7} or ${word8}. When parents go on a vacation, they spend their time eating three ${word9} a day, and fathers play golf, and mothers sit around ${word10}. Last summer, my little brother fell in a/an ${word11} and got poison${word12} all over his${word13}. My family is going to go to (the) ${word14}, and I will practice ${word15}. Parents need vacations more than kids because parents are always very ${word16} and because they have to work${word17} hours every day all year making enough ${word18} to pay for the vacation.`
+
+var paragraph = `A vacation is when you take a trip to some ${word1} place with your ${word2} family. Usually you go to some place that is near a/an ${word3} or up on a/an ${word4}. A good vacation place is one where you can ride ${word5} or play ${word6} or go hunting for ${word7}. I like to spend my time ${word8} or ${word9}. When parents go on a vacation, they spend their time eating three ${word10} a day, and fathers play golf, and mothers sit around ${word11}. Last summer, my little brother fell in a/an ${word12} and got poison${word13} all over his${word14}. My family is going to go to (the) ${word15}, and I will practice ${word16}. Parents need vacations more than kids because parents are always very ${word17} and because they have to work${word18} hours every day all year making enough ${word19} to pay for the vacation.`
 
         document.getElementById('answer').innerHTML = paragraph;
 }
